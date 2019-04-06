dc463dc33cf61e29c1d2c78372236ce0ab4f705a
nagiko <BananaGikoH@users.noreply.github.com>
2019-04-07 04:27:12 +0900

    https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  #https
+  Rails.application.configure do
+    config.force_ssl = true
+   end
 end
